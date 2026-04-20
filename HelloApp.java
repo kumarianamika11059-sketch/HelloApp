e81fe671355eeb2c5bd7b8957bcf006ee2cd1639
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-04-20 09:07:51 +0530

    uc5 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,20 +1,23 @@
-public class HelloApp {
-    public HelloApp() {
-    }
-    public static void main(String[] var0){
-        String var1 = "world";
-        if(var0.length>0){
-        StringBuilder var2 = new StringBuilder();
+//https://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4httphttps://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4https://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4s://github.com/kumarianamika11059-sketch/HelloApp/tree/featpublic class Helloapp_arg {
+    public static void main(String[] args) {
 
-        for(int var3 = 0; var3 < var0.length; ++var3) {
-             var2.append(var0[var3]);
-             if (var3 < var0.length - 1){
-                var2.append(",");
-             }
-        
-        var1 = var2.toString();
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
+
+            System.out.println("Hello, " + names + "!");
         }
     }
-        System.out.println("Hello, " + var1 + "!");
-    }
-}
+
+
