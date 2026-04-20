bf556b1cab31c5abe2e77a877668f3339c26cbc8
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-04-20 09:20:35 +0530

    uc6 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,23 +1,26 @@
 //https://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4httphttps://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4https://github.com/kumarianamika11059-sketch/HelloApp/tree/feature/uc4s://github.com/kumarianamika11059-sketch/HelloApp/tree/featpublic class Helloapp_arg {
+    public class hello {
     public static void main(String[] args) {
 
+        // Case 1: No arguments
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder names = new StringBuilder();
 
-            boolean first = true;
+            StringBuilder nameBuilder = new StringBuilder();
 
+            // Enhanced for loop
             for (String name : args) {
-                if (!first) {
-                    names.append(", ");
-                }
-                names.append(name);
-                first = false;
+                nameBuilder.append(name).append(", ");
+            }
+
+            // Remove trailing ", "
+            String names = "";
+            if (nameBuilder.length() > 0) {
+                names = nameBuilder.substring(0, nameBuilder.length() - 2);
             }
 
             System.out.println("Hello, " + names + "!");
         }
     }
-
-
+}
