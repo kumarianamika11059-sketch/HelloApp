223acaf93b8d00bd1d6747a6957c55cb3407de1d
kumarianamika11059-sketch <kumarianamika11059@gmail.com>
2026-04-20 08:54:19 +0530

    uc4 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,20 @@
 public class HelloApp {
-    public static void main(String[]args){
-        String name = "World";
-        if(args.length>0){
-             name = args[0];
+    public HelloApp() {
+    }
+    public static void main(String[] var0){
+        String var1 = "world";
+        if(var0.length>0){
+        StringBuilder var2 = new StringBuilder();
+
+        for(int var3 = 0; var3 < var0.length; ++var3) {
+             var2.append(var0[var3]);
+             if (var3 < var0.length - 1){
+                var2.append(",");
+             }
+        
+        var1 = var2.toString();
         }
-        System.out.println("Hello, " + name + "!");
+    }
+        System.out.println("Hello, " + var1 + "!");
     }
 }
